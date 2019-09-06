002f5f376580cbb544afe7bd24e3847823fb7b97
Michael George <georgem@wlwv.k12.or.us>
2019-09-06 09:13:16 -0700

    added jaberwocky file

---
+++ Jabberwocky.java
@@ -0,0 +1,14 @@
+
+/**First stanza of Jabberwocky
+ * Write a description of class Jabberwocky here.
+ *
+ * @author (mitchell duea)
+ * @version (1.0)
+ */
+public class Jabberwocky
+{
+    public static void main (String [] args){
+        System.out.print( "'Twas brillig, and the slithy toves /\n" );
+        System.out.print( "Did Gyre and gimble in the wabe" );
+    }
+}
